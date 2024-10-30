0a93326277a907908d1a93323ac4337fe0ff71e4
JBC <jbarretoc12@gmail.com>
2024-10-30 10:10:53 -0500

    Proyecto inicial

---
+++ SISERP/PRINCIPAL/ERP_ACCESO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SISERP.PRINCIPAL
+{
+    public partial class ERP_ACCESO : Form
+    {
+        public ERP_ACCESO()
+        {
+            InitializeComponent();
+        }
+    }
+}
