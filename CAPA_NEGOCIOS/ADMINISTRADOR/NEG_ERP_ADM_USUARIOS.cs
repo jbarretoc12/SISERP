8809f5bf72c706a1cff0b3bdad8f0f08eb082da0
JBC <jbarretoc12@gmail.com>
2024-11-02 11:03:55 -0500

    capa negocios usuarios

---
+++ CAPA_NEGOCIOS/ADMINISTRADOR/NEG_ERP_ADM_USUARIOS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPA_NEGOCIOS.ADMINISTRADOR
+{
+    public class NEG_ERP_ADM_USUARIOS
+    { 
+        public int Opc {  get; set; }
+        public int Opcion { get; set; }
+        public string Criterio { get; set; }
+        public string CoUsu {  get; set; }
+        public string nroDoc { get; set; }
+        public string noUsu { get; set; }
+        public string noClave { get; set; }
+        public string coPerfil { get; set; }
+        public string estado { get; set; }
+        public string co_usua_crea { get; set; }
+
+    }
+}
